789fd806dbc10a176060a9e47523fc3b24c579b6
nathan78906 <nathen78906@gmail.com>
2018-06-10 14:30:44 -0400

    better final list output

--- app.py
+++ app.py
@@ -14,16 +14,18 @@ with open('greenhouse.json') as json_data:
 with open('lever.json') as json_data:
     lever = json.load(json_data)
 
-email_list = ""
+email_list = []
 
 for g in greenhouse:
 	try:
 		response = requests.get(g["url"], timeout=2)
 	except requests.exceptions.Timeout:
 		print("Timeout on: " + g["url"])
+		email_list.append("Timeout on: " + g["url"])
 		continue
 	except requests.exceptions.ConnectionError:
 		print("Connection Error on: " + g["url"])
+		email_list.append("Connection Error on: " + g["url"])
 		continue
 
 	if response.status_code != 200:
@@ -32,7 +34,7 @@ for g in greenhouse:
 	print(g["url"])
 	for job in response.json()["jobs"]:
 		if any([x in job["title"].lower() for x in filter_words]) and not any([x in job["title"].lower() for x in blacklist]):
-			email_list += g["name"] + " - " + job["title"] + ": " + job["absolute_url"] + " \n\n"
+			email_list.append(g["name"] + " - " + job["title"] + ": " + job["absolute_url"])
 
 
 for l in lever:
@@ -40,9 +42,11 @@ for l in lever:
 		response = requests.get(l["url"], timeout=2)
 	except requests.exceptions.Timeout:
 		print("Timeout on: " + l["url"])
+		email_list.append("Timeout on: " + l["url"])
 		continue
 	except requests.exceptions.ConnectionError:
 		print("Connection Error on: " + l["url"])
+		email_list.append("Connection Error on: " + l["url"])
 		continue
 
 	if response.status_code != 200:
@@ -51,7 +55,8 @@ for l in lever:
 	print(l["url"])
 	for job in response.json():
 		if any([x in job["text"].lower() for x in filter_words]) and not any([x in job["text"].lower() for x in blacklist]):
-			email_list += l["name"] + " - " + job["text"] + ": " + job["hostedUrl"] + " \n\n"
+			email_list.append(l["name"] + " - " + job["text"] + ": " + job["hostedUrl"])
+
 
 now = datetime.now()
 
@@ -60,7 +65,7 @@ sg = sendgrid.SendGridAPIClient(apikey=os.environ['SENDGRID_API_KEY'])
 from_email = Email("intern@jobs.com", "InternTracker")
 to_email = Email(os.environ['TO_EMAIL'])
 subject = "Internships & Co-ops" + " - " + now.strftime("%x")
-content = Content("text/plain", email_list)
+content = Content("text/plain", "\n\n".join(email_list))
 mail = Mail(from_email, subject, to_email, content)
 response = sg.client.mail.send.post(request_body=mail.get())
 print(response.status_code)
